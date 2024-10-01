10f8ff10a79d98c455b2b60f02c617f348edc932
Alex <alex2323official@gmail.com>
2024-10-01 15:19:52 +0200

    Added Prettier as project dependency

--- src/greeting.js
+++ src/greeting.js
@@ -1,8 +1,8 @@
 // Example with linting issues
-let unusedVar = 10
+let unusedVar = 10;
 
 function greet(name) {
-    console.log("Hello, " + name)
+  console.log("Hello, " + name);
 }
 
-greet("World")
+greet("World");
